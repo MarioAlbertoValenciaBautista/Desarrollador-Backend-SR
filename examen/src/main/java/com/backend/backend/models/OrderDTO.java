77fe74c6a4eac02b31c08a97fe234d1c472890e1
MarioAlbertoValenciaBautista <mariovalenciabautista@gmail.com>
2025-08-03 17:05:45 -0600

    initialize project

---
+++ examen/src/main/java/com/backend/backend/models/OrderDTO.java
@@ -0,0 +1,66 @@
+package com.backend.backend.models;
+
+
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Table(name = "order")
+public class OrderDTO {
+
+
+    private Long id;
+    private String status;
+    private String origin;
+    private String destination;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
